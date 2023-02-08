2e3927c7dca2ec3193e855a60ae39feb902899a5
David Li <davidli012345@gmail.com>
2023-02-07 19:39:10 -0800

    fix: update dart off server

--- bin/dart_off_server.dart
+++ bin/dart_off_server.dart
@@ -1,6 +1,7 @@
-import 'package:dart_off_server/core.dart' as cli;
+import 'package:dart_off_server/core.dart' as openfood;
 import 'package:alfred/alfred.dart';
 import 'dart:io';
+import 'package:openfoodfacts/openfoodfacts.dart';
 
 void main(List<String> arguments) async  {
   // get port from arguments
@@ -10,12 +11,16 @@ void main(List<String> arguments) async  {
   } else {
     port = int.tryParse(arguments.first) ?? 6565;
   }
+  openfood.mkConfiguration();
   final app = Alfred();
 
   // print line
   // print('Starting up server: on port $port');
 
-  app.get('/', (req, res) => {'json_response': true});
+  app.get('/', (req, res) {
+    res.headers.contentType = ContentType.html;
+    return '<html><body><h1>Test HTML</h1></body></html>';
+  });
   app.get('/text', (req, res) => 'Text response');
 
   app.get('/json', (req, res) => {'json_response': true});
@@ -33,6 +38,15 @@ void main(List<String> arguments) async  {
    app.post('/post-route', (req, res) async {
     final body = await req.body; //JSON body
     body != null; //true
+  });
+
+  app.get('/food/search', (req, res) {
+    final query = req.uri.queryParameters;
+    return openfood.search(query).then((value) {
+      res.json(value);
+    }).catchError((error) {
+      res.json(error);
+    });
   });
 
   await app.listen(port); //Listening on port 6565
--- lib/core.dart
+++ lib/core.dart
@@ -1,3 +1,33 @@
-int calculate() {
-  return 6 * 7;
+import 'package:openfoodfacts/openfoodfacts.dart';
+
+Future<SearchResult> search(Map<String, String> query) {
+  var parametersList = <Parameter>[];
+  // check for terms in quer
+  if (query.containsKey('terms')) {
+    // print('terms: ${query['terms']}');
+    var terms = query['terms'];
+    if (terms != null && terms.isNotEmpty) {
+      parametersList.add(SearchTerms(terms: terms.split(',')));
+    }
+  }
+  ProductSearchQueryConfiguration configuration =
+      ProductSearchQueryConfiguration(
+    parametersList: parametersList,
+    version: ProductQueryVersion.v3,
+  );
+  return OpenFoodAPIClient.searchProducts(
+    User(userId: '', password: ''),
+    configuration,
+  );
+}
+
+void mkConfiguration() {
+  OpenFoodAPIConfiguration.userAgent = UserAgent(
+      name: 'dart_off_server',
+      url: 'https://friendlyuser-dart-off-server.hf.space/');
+  OpenFoodAPIConfiguration.globalLanguages = <OpenFoodFactsLanguage>[
+    OpenFoodFactsLanguage.ENGLISH
+  ];
+
+  OpenFoodAPIConfiguration.globalCountry = OpenFoodFactsCountry.CANADA;
 }
